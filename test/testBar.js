23285198cbc8d30d1207a3ceada8e6f661ca4423
Jean-Marc Skopek <jskopek@gmail.com>
2012-02-23 17:34:16 -0500

    Added additional test case

--- test/testBar.js
+++ test/testBar.js
@@ -1,16 +1,29 @@
 $(document).ready(function() {
     module("Bar");
     test("initialize", function() {
-        var data = [];
-        $("#bar").barReporter({ data: data });
+        var el = $("<div class='bar'></div>");
+        $(el).barReporter({ data: [] });
     });
     test("render simple bar", function() {
-        $("#bar").barReporter({ "data": [[30, "Label 1"]]});
-        ok( $("#bar .brRow").length, 1 );
-        equal( $("#bar #brRow0 #brBar0")[0].style.width, "100%" );
-        ok( $("#bar #brRow0 .brLabel").filter(function() { return $(this).text() == "Label 1" }).length );
+        var el = $("<div class='bar'></div>");
+
+        $(el).barReporter({ "data": [[30, "Label 1"]]});
+        ok( $(el).find(".brRow").length, 1 );
+        equal( $(el).find("#brRow0 #brBar0")[0].style.width, "100%" );
+        ok( $(el).find("#brRow0 .brLabel").filter(function() { return $(this).text() == "Label 1" }).length );
     });
     test("render multiple bars", function() {
+        var el = $("<div class='bar'></div>");
+        var data = [[20, "Label 1"], [40, "Label 2"]];
+        $(el).barReporter({"data": data});
+
+        ok( $(el).find(".brRow").length, 2 );
+
+        equal( $(el).find("#brRow0 #brBar0")[0].style.width, "50%" );
+        ok( $(el).find("#brRow0 .brLabel").filter(function() { return $(this).text() == "Label 1" }).length );
+
+        equal( $(el).find("#brRow1 #brBar0")[0].style.width, "100%" );
+        ok( $(el).find("#brRow1 .brLabel").filter(function() { return $(this).text() == "Label 2" }).length );
     });
     test("simple update", function() {
     });
